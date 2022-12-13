b9a2f8741d5f66aa8e61ebda282678ebe3052334
raoaamir <muhammad.amir@gigalabs.co>
2022-12-13 19:52:34 +0500

    Donegit add .

---
+++ controllers/authController.js
@@ -0,0 +1,88 @@
+const { json } = require('express')
+const User = require('../models/user.js')
+const jwt = require('jsonwebtoken')
+
+const handlingError = (err)=>{
+    console.log(err.message , err.code)
+    let errors = {email : '' , password : ''}
+
+    if(err.message === 'Incorrect Email'){
+        errors.email = 'This email is not registered'
+    }
+
+    if(err.message === 'Incorrect Password'){
+        errors.email = 'This password is not correct'
+    }
+    
+
+    //Dublicate Error
+
+    if(err.code === 11000){
+        errors.email = ('Email is Already Exist')
+        return errors
+    }
+
+    //validation error
+
+    if(err.message.includes('user validation failed')){
+        Object.values(err.errors).forEach(({properties})=>{
+            errors[properties.path] = properties.message
+        })
+    }
+    return errors;
+}
+
+const maxAge = 3 * 24 * 60 * 60
+const createToken = (id) =>{
+    return jwt.sign({id} , 'this is my secret key ' ,{
+        expiresIn :maxAge
+    } )
+}
+// controller actions
+module.exports.signup_get = (req, res) => {
+    res.render('signup');
+  }
+  
+  module.exports.login_get = (req, res) => {
+    res.render('login');
+  }
+  
+  module.exports.signup_post = async(req, res) => {
+    const {email ,password} = req.body;
+    try {
+        const user = await User.create({email ,password})
+        const token = createToken(user._id)
+        res.cookie('jwt' , token ,{httpOnly : true , maxAge : maxAge*1000})
+        res.status(201).json({user : user._id})
+        
+    } catch (err) {
+        const errors = handlingError(err)
+        res.status(400).json({errors})
+        
+    }
+  }
+  
+  module.exports.login_post = async (req, res) => {
+    const {email ,password} = req.body;
+
+    try {
+
+        const user =await User.login(email , password)
+        const token = createToken(user._id)
+        res.cookie('jwt' , token ,{httpOnly : true , maxAge : maxAge*1000})
+        console.log({user})
+        res.status(200).json({user :user._id})
+        
+    } catch (err) {
+        const errors = handlingError(err)
+        res.status(400).json({errors})
+        
+    }
+}
+
+  module.exports.logout_get = (req ,res)=>{
+    res.cookie('jwt' , '' ,{maxAge : 1})
+    res.redirect('/')
+
+  }
+  
